9ffd82447f7b65f83db5a0d893e94902250286b9
sandesh050 <sandeshgupta0509@gmail.com>
2025-04-28 13:56:17 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Import Firebase app and Firestore
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIgRZCMqbRxo7jhYJCwVoIz3re6L_g8GM",
+  authDomain: "expense-tracker-d5631.firebaseapp.com",
+  projectId: "expense-tracker-d5631",
+  storageBucket: "expense-tracker-d5631.firebasestorage.app",
+  messagingSenderId: "336895637396",
+  appId: "1:336895637396:web:f8a98f8a17ec6cf70a8181"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
 
